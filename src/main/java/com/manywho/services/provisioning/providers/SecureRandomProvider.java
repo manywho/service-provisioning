c1843ffcc050294d87c3bba58a172ddf3164e9e3
Jonjo McKay <jonjo@jonjomckay.com>
2016-12-18 16:04:13 +0000

    Made SecureRandomProvider a single with an annotation

--- src/main/java/com/manywho/services/provisioning/providers/SecureRandomProvider.java
+++ src/main/java/com/manywho/services/provisioning/providers/SecureRandomProvider.java
@@ -2,8 +2,10 @@ package com.manywho.services.provisioning.providers;
 
 import com.google.inject.Provider;
 
+import javax.inject.Singleton;
 import java.security.SecureRandom;
 
+@Singleton
 public class SecureRandomProvider implements Provider<SecureRandom> {
     @Override
     public SecureRandom get() {
