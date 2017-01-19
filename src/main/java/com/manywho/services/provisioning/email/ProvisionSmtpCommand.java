6ea1f0f1dc80f62cd8c9405edfe309c0d1fbe53a
Jose Collazzi <jose.collazzi@manywho.com>
2017-01-19 18:40:45 +0000

    added more validations and verification to the email address of te user

--- src/main/java/com/manywho/services/provisioning/email/ProvisionSmtpCommand.java
+++ src/main/java/com/manywho/services/provisioning/email/ProvisionSmtpCommand.java
@@ -5,16 +5,21 @@ import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClient;
 import com.amazonaws.services.simpleemail.model.ListVerifiedEmailAddressesResult;
 import com.amazonaws.services.simpleemail.model.VerifyEmailAddressRequest;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.manywho.sdk.api.run.elements.config.ServiceRequest;
 import com.manywho.sdk.services.actions.ActionCommand;
 import com.manywho.sdk.services.actions.ActionResponse;
 import com.manywho.services.provisioning.ApplicationConfiguration;
 import com.manywho.services.provisioning.ServiceConfiguration;
 import com.manywho.services.provisioning.factories.AwsFactory;
+import org.hibernate.validator.internal.constraintvalidators.hv.EmailValidator;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import javax.inject.Inject;
 import javax.xml.bind.DatatypeConverter;
+import java.io.IOException;
 
 public class ProvisionSmtpCommand implements ActionCommand<ServiceConfiguration, ProvisionSmtp, ProvisionSmtp.Input, ProvisionSmtp.Output> {
     private final ApplicationConfiguration applicationConfiguration;
@@ -28,7 +33,13 @@ public class ProvisionSmtpCommand implements ActionCommand<ServiceConfiguration,
 
     @Override
     public ActionResponse<ProvisionSmtp.Output> execute(ServiceConfiguration configuration, ServiceRequest request, ProvisionSmtp.Input input) {
-        verifyEmailAddress(input.getTenantEmail());
+        // remove whitespaces and * that have a special meaning in amazon and can be used as a reg exp
+        String cleanEmail = input.getTenantEmail().replace("*", "").trim();
+
+        validateEmailAddress (cleanEmail);
+
+        // Create a new policyDocument with the policy template and the email address of the user
+        String policyDocument = getPolicyString(cleanEmail);
 
         // We want to create an IAM user that is scoped to tenants
         String iamUsername = "tenant-" + input.getTenant();
@@ -37,7 +48,7 @@ public class ProvisionSmtpCommand implements ActionCommand<ServiceConfiguration,
         CreateUserResult createUserResult = awsFactory.getIamClient().createUser(new CreateUserRequest(iamUsername));
 
         // Add policy to user
-        addPolicyEmailRestrictionToUser(createUserResult.getUser().getUserName(), input.getTenantEmail());
+        addPolicyEmailRestrictionToUser(createUserResult.getUser().getUserName(), policyDocument);
 
         // Add the IAM user to the desired SES group
         awsFactory.getIamClient().addUserToGroup(new AddUserToGroupRequest(applicationConfiguration.getAwsTenantGroup(), iamUsername));
@@ -50,6 +61,9 @@ public class ProvisionSmtpCommand implements ActionCommand<ServiceConfiguration,
         String username = accessKey.getAccessKeyId();
         String password = generateSmtpPassword(accessKey.getSecretAccessKey());
 
+        // send a verification link to the user by email
+        verifyEmailAddress(cleanEmail);
+
         return new ActionResponse<>(new ProvisionSmtp.Output(
                 applicationConfiguration.getSmtpHostname(),
                 "tls",
@@ -59,10 +73,18 @@ public class ProvisionSmtpCommand implements ActionCommand<ServiceConfiguration,
         ));
     }
 
-    private void addPolicyEmailRestrictionToUser(String userName, String tenantEmail) {
+    private void validateEmailAddress(String tenantEmail) {
+        EmailValidator emailValidator = new EmailValidator();
+
+        if(! emailValidator.isValid(tenantEmail, null)) {
+            throw new RuntimeException("The email is not valid");
+        }
+    }
+
+    private void addPolicyEmailRestrictionToUser(String userName, String policyDocument) {
         CreatePolicyRequest createPolicyRequest = new CreatePolicyRequest();
         createPolicyRequest.setPolicyName(userName);
-        createPolicyRequest.setPolicyDocument("{\"Version\": \"2012-10-17\", \"Statement\": [{\"Effect\": \"Allow\", \"Action\": [\"ses:SendEmail\", \"ses:SendRawEmail\"],\"Resource\":\"*\", \"Condition\": {\"StringEquals\": {\"ses:FromAddress\": \"" + tenantEmail + "\"}}}]}");
+        createPolicyRequest.setPolicyDocument(policyDocument);
         CreatePolicyResult createPolicyResult =  awsFactory.getIamClient().createPolicy(createPolicyRequest);
 
         AttachUserPolicyRequest attachUserPolicyRequest = new AttachUserPolicyRequest();
@@ -70,6 +92,21 @@ public class ProvisionSmtpCommand implements ActionCommand<ServiceConfiguration,
         attachUserPolicyRequest.setUserName(userName);
         awsFactory.getIamClient().attachUserPolicy(attachUserPolicyRequest);
 
+    }
+
+
+    public String getPolicyString(String tenantEmail) {
+        ObjectMapper mapper = new ObjectMapper();
+        String policyTemplate = "{\"Version\": \"2012-10-17\", \"Statement\": [{\"Effect\": \"Allow\", \"Action\": [\"ses:SendEmail\", \"ses:SendRawEmail\"],\"Resource\":\"*\", \"Condition\": {\"StringEquals\": {\"ses:FromAddress\": \"to-replace\"}}}]}";
+        try {
+            JsonNode root = mapper.readTree(policyTemplate);
+            JsonNode fromRestriction = root.path("Statement").path(0).path("Condition").path("StringEquals");
+            ((ObjectNode) fromRestriction).put("ses:FromAddress", tenantEmail);
+
+            return root.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void verifyEmailAddress(String address) {
